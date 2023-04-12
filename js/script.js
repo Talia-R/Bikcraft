5f32809e6b7f28d795846830d63a45cce2124903
Natalia <natalia.1067@hotmail.com>
2023-04-12 15:20:12 -0300

    javascrip

---
+++ js/script.js
@@ -0,0 +1,68 @@
+// Ativar Links do menu
+const links = document.querySelectorAll(".navegacao-primaria a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ativar itens do orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  elemento.checked = true;
+  console.log(elemento);
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas frequentes abrir/fechar
+
+const perguntas = document.querySelectorAll("#seguros .perguntas button");
+
+function abrir(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", abrir);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// Galeria
+
+const galeria = document.querySelectorAll(".bicicletas-imagens img");
+const galeriaContainer = document.querySelector(".bicicletas-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
